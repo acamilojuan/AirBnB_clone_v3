ba2d00a70a99e25723e844da9f840988be1f8967
acamilojuan <1600@holbertonschool.com>
2020-09-27 13:47:25 -0500

    Semistandard style fixed

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
-    let amenitieCheckBox = $('DIV.amenities DIV.popover ul li input');
-    amenitieCheckBox.css('margin-right', '10px');
-    let datos = {};
-    $('input[type=checkbox]').on('click', function () {
-        if ($(this).prop('checked') == true){
-            datos[$(this).attr('data-name')] = $(this).attr('data-id');
-        }
-        else{
-            delete datos[$(this).attr('data-name')];
-        }
-        $('DIV.amenities h4').text(Object.keys(datos).join(', '));
-    });   
+  const amenitieCheckBox = $('DIV.amenities DIV.popover ul li input');
+  amenitieCheckBox.css('margin-right', '10px');
+  const datos = {};
+  $('input[type=checkbox]').on('click', function () {
+    if ($(this).prop('checked') === true) {
+      datos[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else {
+      delete datos[$(this).attr('data-name')];
+    }
+    $('DIV.amenities h4').text(Object.keys(datos).join(', '));
+  });
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,25 +1,23 @@
 $(document).ready(function () {
-    let amenitieCheckBox = $('DIV.amenities DIV.popover ul li input');
-    amenitieCheckBox.css('margin-right', '10px');
-    let datos = {};
-    $('input[type=checkbox]').on('click', function () {
-        if ($(this).prop('checked') == true){
-            datos[$(this).attr('data-name')] = $(this).attr('data-id');
-        }
-        else{
-            delete datos[$(this).attr('data-name')];
-        }
-        $('DIV.amenities h4').text(Object.keys(datos).join(', '));
-    });
-    
-    $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
-        console.log(data.status);
-        if (data.status === 'OK') {
-            console.log('prendido')
-            $('DIV#api_status').toggleClass('available');
-        }
-        else {
-            $('DIV#api_status').toggleClass('unavailable');
-        }
-    })
+  const amenitieCheckBox = $('DIV.amenities DIV.popover ul li input');
+  amenitieCheckBox.css('margin-right', '10px');
+  const datos = {};
+  $('input[type=checkbox]').on('click', function () {
+    if ($(this).prop('checked') === true) {
+      datos[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else {
+      delete datos[$(this).attr('data-name')];
+    }
+    $('DIV.amenities h4').text(Object.keys(datos).join(', '));
+  });
+
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    console.log(data.status);
+    if (data.status === 'OK') {
+      console.log('prendido');
+      $('DIV#api_status').toggleClass('available');
+    } else {
+      $('DIV#api_status').toggleClass('unavailable');
+    }
+  });
 });
