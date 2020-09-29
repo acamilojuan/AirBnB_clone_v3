8f0c98f7e5d2e0830c805f4f40a68d3dd4634e6b
Sainterman <sancastillog@gmail.com>
2020-09-28 21:40:59 -0500

    task 4 missing owner name, api/v1/users/<user_id> doesnt returns specific user

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -12,12 +12,35 @@ $(document).ready(function () {
   });
 
   $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    console.log(data.status);
     if (data.status === 'OK') {
-      console.log('prendido');
       $('DIV#api_status').toggleClass('available');
     } else {
       $('DIV#api_status').toggleClass('unavailable');
     }
+  });
+
+  url = 'http://0.0.0.0:5001/api/v1/places_search/';
+  $.ajax({
+    type:"POST",
+    url: url,
+    data: JSON.stringify({}),
+    success: function (data) {
+      console.log(data)
+      data.forEach(place => {
+        let guests = place.max_guest === 1 ? 'Guest' : 'Guests';
+        let bedrooms = place.number_rooms === 1 ? 'Bedroom' : 'Bedrooms';
+        let bathrooms = place.number_bathrooms === 1 ? 'Bathroom': 'Bathrooms';
+        let tittle_box = '<div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">' + '$' +place.price_by_night+'</div></div>';
+        let information = '<div class="information"><div class="max_guest">'+
+        place.max_guest + ' ' + guests + '</div><div class="number_rooms">'
+        + place.number_rooms + ' ' + bedrooms + '</div><div class="number_bathrooms">' +
+        place.number_bathrooms + ' ' + bathrooms + '</div></div>';
+        let description = '<div class="description">' + place.description + '</div>';
+        let content = tittle_box + information + description;
+        $('SECTION.places').append('<article>' + content + '</article>');
+
+      });
+    },
+    contentType: 'application/json; charset=utf-8'
   });
 });
